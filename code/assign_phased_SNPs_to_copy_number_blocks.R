998b56960bbdd2f14d7e30063bbc91d0517b2e88
nicola <casiraghinicola@gmail.com>
2019-10-18 15:38:05 +0200

    minor changes

--- code/assign_phased_SNPs_to_copy_number_blocks.R
+++ code/assign_phased_SNPs_to_copy_number_blocks.R
@@ -5,8 +5,8 @@ haplotype_blocks <- "/icgc/dkfzlsdf/analysis/B260/projects/chromothripsis_medull
 
 phased_snps <- "/icgc/dkfzlsdf/analysis/B260/projects/chromothripsis_medulloblastoma/whatshap_phasing/outs/tmp_analysis/phased_Hg19_Nanopore.sort.noChr.OnlyPhased.vcf"
 
-# setwd('/icgc/dkfzlsdf/analysis/B260/projects/chromothripsis_medulloblastoma/whatshap_phasing/outs/copy_number_blocks_10xCNV_data/12_clusters_362cells/')
-setwd('/icgc/dkfzlsdf/analysis/B260/projects/chromothripsis_medulloblastoma/whatshap_phasing/outs/copy_number_blocks_10xCNV_data/9_clusters_362cells/')
+setwd('/icgc/dkfzlsdf/analysis/B260/projects/chromothripsis_medulloblastoma/whatshap_phasing/outs/copy_number_blocks_10xCNV_data/12_clusters_362cells/')
+# setwd('/icgc/dkfzlsdf/analysis/B260/projects/chromothripsis_medulloblastoma/whatshap_phasing/outs/copy_number_blocks_10xCNV_data/9_clusters_362cells/')
 # setwd('/icgc/dkfzlsdf/analysis/B260/projects/chromothripsis_medulloblastoma/whatshap_phasing/outs/copy_number_blocks_GTseq_data/')
 
 # intersect haplotype blocks and phased snps
@@ -26,7 +26,8 @@ summary(x$V3-x$V2)
 # scdna_clusters <- read.csv("/icgc/dkfzlsdf/analysis/B260/users/v390v/cnv_inference/data/raw/first_sample/scDNA/evo_dist_9_clustering.csv")
 # scdna_clusters <- scdna_clusters[,2:4]
 
-scdna_clusters <- read.delim("/icgc/dkfzlsdf/analysis/B260/projects/chromothripsis_medulloblastoma/data/10xCNV/STP/STP-Nuclei/processed_data/cluster9_cell_ids.txt")
+# scdna_clusters <- read.delim("/icgc/dkfzlsdf/analysis/B260/projects/chromothripsis_medulloblastoma/data/10xCNV/STP/STP-Nuclei/processed_data/cluster9_cell_ids.txt")
+scdna_clusters <- read.delim("/icgc/dkfzlsdf/analysis/B260/projects/chromothripsis_medulloblastoma/data/10xCNV/STP/STP-Nuclei/processed_data/cluster12_cell_ids.txt")
 
 scdna_clusters <- scdna_clusters[,2:3]
 colnames(scdna_clusters) <- c("CELL_ID","LABEL")
@@ -129,7 +130,7 @@ sum(row.has.na)/nrow(a)
 
 hb_to_keep <- a[!row.has.na,]
 
-for(k in seq(2,length(ks))){
+for(k in seq_len(length(ks))){
   message(k)
   
   # step 6 : filter haplotype blocks
